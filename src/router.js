161c0e59d3cfb0d5d49f5191204c8cf5419ae949
刘龙宾 <liulongbin1314@outlook.com>
2019-02-27 11:55:20 +0800

    完成用户列表功能的开发

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Login from './components/Login.vue'
 import Home from './components/Home.vue'
 import Welcome from './components/Welcome.vue'
+import Users from './components/user/Users.vue'
 
 Vue.use(Router)
 
@@ -14,7 +15,10 @@ const router = new Router({
       path: '/home',
       component: Home,
       redirect: '/welcome',
-      children: [{ path: '/welcome', component: Welcome }]
+      children: [
+        { path: '/welcome', component: Welcome },
+        { path: '/users', component: Users }
+      ]
     }
   ]
 })
